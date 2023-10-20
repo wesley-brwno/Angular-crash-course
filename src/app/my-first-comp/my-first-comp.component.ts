eaa132ac1a74a5333311e6548ee4ffb4029b2a5d
wesley <wesleybruno910@gmail.com>
2023-10-20 12:20:01 -0300

    creating a simple form

---
+++ src/app/my-first-comp/my-first-comp.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-first-comp',
+  templateUrl: './my-first-comp.component.html',
+  styleUrls: ['./my-first-comp.component.scss']
+})
+export class MyFirstCompComponent {
+
+  name: string = '';
+  email: string =  '';
+  message: string = '';
+
+  onSubmit(): void {
+    console.log(this.name);
+  }
+}
