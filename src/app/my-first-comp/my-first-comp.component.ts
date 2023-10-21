7ceceea8b9717df1fc1b7d57701ceceae1094ba1
wesley <wesleybruno910@gmail.com>
2023-10-21 12:25:00 -0300

    Dependency Injection

--- src/app/my-first-comp/my-first-comp.component.ts
+++ src/app/my-first-comp/my-first-comp.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MyFirstService } from '../services/my-first.service';
 
 @Component({
   selector: 'app-my-first-comp',
@@ -7,11 +8,31 @@ import { Component } from '@angular/core';
 })
 export class MyFirstCompComponent {
 
+
   name: string = '';
   email: string =  '';
   message: string = '';
+  isSubmitted: boolean = false;
+  messages: Array<any> = [];
+
+  constructor(
+    private service: MyFirstService
+  ) {
+    this.messages = this.service.getAllMessages();
+    this.isSubmitted = this.messages.length > 0;
+  }
 
   onSubmit(): void {
-    console.log(this.name);
+    this.isSubmitted = true;
+    this.service.insert({
+      'name' : this.name,
+      'email' : this.email,
+      'message' : this.message
+    });
+    console.log(this.messages);
+  }
+
+  deleteMessage(index: number): void {
+    this.service.deleteMessage(index);
   }
 }
---
+++ src/app/services/my-first.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MyFirstService {
+
+  messages: Array<any> = [];
+
+  constructor() {
+    this.init();
+  }
+
+  init(): void {
+    this.insert(
+    {
+      name: 'Wesley',
+      email: 'wesley@email.com',
+      message: 'qq coisa ....'
+    });
+    this.insert(
+    {
+      name: 'João ',
+      email: 'joãoy@email.com',
+      message: 'qq coisa 2 ....'
+    });
+    this.insert(
+    {
+      name: 'Tanjiro',
+      email: 'tanjiro@email.com',
+      message: 'qq coisa 3 ....'
+    });
+  }
+
+  insert(message: {name: string, email: string, message: string}): void {
+    this.messages.push(message);
+  }
+
+  getAllMessages(): Array<any> {
+    return this.messages;
+  }
+
+  deleteMessage(index: number): void {
+    this.messages.splice(index, 1);
+  }
+}
